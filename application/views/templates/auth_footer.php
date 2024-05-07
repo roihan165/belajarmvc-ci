936eb9d6129d9a340c8836951bd6aead8664b7a5
Roy_kimochi <Roy@gmail.com>
2024-05-07 18:20:53 +0700

    Pertemuan7 Finished

---
+++ application/views/templates/auth_footer.php
@@ -0,0 +1,8 @@
+    <script src="<?= base_url('vendor/')?>startbootstrap-sb-admin-2-gh-pages/vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('vendor/')?>startbootstrap-sb-admin-2-gh-pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url('vendor/')?>startbootstrap-sb-admin-2-gh-pages/vendor/jquery-easing/jquery-easing.min.js"></script>
+    <script src="<?= base_url('vendor/')?>startbootstrap-sb-admin-2-gh-pages/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
