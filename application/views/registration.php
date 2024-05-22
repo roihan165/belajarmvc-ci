f359fe6294c5eebad24130bfec507eb6f9d20f72
Amil561 <amilmaaruf@gmail.com>
2024-05-22 18:52:57 +0700

    update dari materi 9

--- application/views/registration.php
+++ application/views/registration.php
@@ -1,33 +1,39 @@
 <div class="container">
-        <div class="col-lg-7">
-            <div class="card o-hidden border-0 shadow-lg my-5">
-                <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-lg">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Registration Page</h1>
+        <div class="card 0-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
+            <div class="card-body p-0">
+                <!-- Nested row within card body -->
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="p-5">
+                            <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Create an Account</h1>
                                 </div>
-                                <form action="" class="user">
+                                <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
                                     <div class="form-group">
-                                        <input type="text" name="name" id="name" class="form-control form-control-user" placeholder="Full Name">
+                                        <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name" value="<?= set_value('name')?>">
+
+                                        <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group">
-                                        <input type="text" name="email" id="email" class="form-control form-control-user" placeholder="Email Address">
+                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email');?>">
+
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group row">
                                         <div class="col-sm-6 mb-3 mb-sm-0">
-                                            <input type="password" name="password" id="password" class="form-control form-control-user" placeholder="Password">
+                                            <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="password">
+
+                                            <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                                         </div>
                                         <div class="col-sm-6">
-                                            <input type="password" name="password2" id="password2" class="form-control form-control-user" placeholder="Repeat Password">
+                                            <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
                                         </div>
                                     </div>
                                     <button type="submit" class="btn btn-primary btn-user btn-block">Register Accoutn</button>
                                 </form>
                                 <hr>
-                                <div class="text-center"><a href="<?= base_url('auth/forgot_password');?>" class="small">Forgot Password</a></div>
-                                <div class="text-center"><a href="<?= base_url('auth/');?>" class="small">Already have an account? Login!</a></div>
+                                <div class="text-center"><a class="small" href="forgot-password.html">Forgot Password?</div>
+                                <div class="text-center"><a class="small" href="<?= base_url('auth/');?>" >Already have an account? Login!</a></div>
                             </div>
                         </div>
                     </div>
