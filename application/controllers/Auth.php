44db485ec3a700ce577f8d3690ee15120b9d67f1
Amil561 <amilmaaruf@gmail.com>
2024-05-29 10:45:01 +0700

    pertemuan 10 gaes

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -50,7 +50,11 @@ class Auth extends CI_Controller
                         'role_id' => $user['role_id']
                     ];
                     $this->session->set_userdata($data);
-                    redirect('user');
+                    if($user['role_id']==1){
+                        redirect('admin');
+                    }else{
+                        redirect('user');
+                    }
                 } else {
                     //jika passwordnya salah
                     $this->session->set_flashdata(
